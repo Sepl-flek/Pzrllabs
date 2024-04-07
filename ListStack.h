56c6739d4ad3986ab3a8f77aa8c431e1bd1a9bc6
VSEVOLOD <seva.polunin@mail.ru>
2024-04-07 11:51:28 +0300

    Description of classes

--- ListStack.h
+++ ListStack.h
@@ -4,5 +4,18 @@
 
 class ListStack : public IStackImplementation
 {
-
+    public:
+    void push(const ValueType& value) override;
+    // удаление с хвоста
+    void pop() override;
+    // посмотреть элемент в хвосте
+    const ValueType& top() const override;
+    // проверка на пустоту
+    bool isEmpty() const override;
+    // размер 
+    size_t size() const override;
+    // виртуальный деструктор
+    
+    private:
+    std::list<ValueType> data;
 };
